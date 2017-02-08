c6ae7320ce75b58e74466a57d7fd3b84eb3fd71a
manni19682 <manfred.thein@sealsystems.de>
2017-02-08 17:18:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# seal-stream-as-string
+
+seal-stream-as-string transforms streams to strings.
+
+## Installation
+
+```bash
+$ npm install seal-stream-as-string
+```
+
+## Quick start
+
+First you need to add a reference to seal-stream-as-string within your application.
+
+```javascript
+const StreamAsString = require('seal-stream-as-string');
+```
+
+Then use the `write` function to add data to the stream. Once you are done, call `end`. Afterwards, you may use the `asString` function to get the stringified stream.
+
+```javascript
+const streamAsString = new StreamAsString();
+
+streamAsString.write('foo');
+streamAsString.write('bar');
+streamAsString.end();
+
+console.log(streamAsString.asString()); // => 'foobar'
+```
+
+Alternatively, you may listen to the `as-string` event. It is raised after the stream has been ended.
+
+```javascript
+const streamAsString = new StreamAsString();
+
+streamAsString.once('as-string', (asString) => {
+  console.log(asString); // => 'foobar'
+});
+
+streamAsString.write('foo');
+streamAsString.write('bar');
+streamAsString.end();
+```
+
+## Running the build
+
+To build this module use [roboter](https://www.npmjs.com/package/roboter).
+
+```bash
+$ bot
+```
---
+++ test/StreamAsStringTest.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const stream = require('stream');
+
+const assert = require('assertthat');
+
+const StreamAsString = require('../lib/StreamAsString');
+
+const Transform = stream.Transform;
+
+suite('StreamAsString', () => {
+  let streamAsString;
+
+  setup(() => {
+    streamAsString = new StreamAsString();
+  });
+
+  test('is a function.', (done) => {
+    assert.that(StreamAsString).is.ofType('function');
+    done();
+  });
+
+  test('returns a transform stream.', (done) => {
+    assert.that(streamAsString).is.instanceOf(Transform);
+    done();
+  });
+
+  test('raises an as-string event.', (done) => {
+    streamAsString.once('as-string', (asString) => {
+      assert.that(asString).is.equalTo('foobar');
+      done();
+    });
+
+    streamAsString.write('foo');
+    streamAsString.write('bar');
+    streamAsString.end();
+  });
+
+  suite('asString', () => {
+    test('is a function.', (done) => {
+      assert.that(streamAsString.asString).is.ofType('function');
+      done();
+    });
+
+    test('throws an exception if called before end.', (done) => {
+      assert.that(() => {
+        streamAsString.asString();
+      }).is.throwing('Must be called after end.');
+      done();
+    });
+
+    test('returns an empty string if write was not called.', (done) => {
+      streamAsString.end();
+      assert.that(streamAsString.asString()).is.equalTo('');
+      done();
+    });
+
+    test('returns the stringified stream.', (done) => {
+      streamAsString.write('foo');
+      streamAsString.write('bar');
+      streamAsString.end();
+      assert.that(streamAsString.asString()).is.equalTo('foobar');
+      done();
+    });
+  });
+});
